b8952cc152a68ae198a6c6831f7c66e32efb2676
C-D-Lewis <Bonsitm@gmail.com>
2015-09-22 22:35:14 +0100

    Update for changed graphics APIs

--- src/windows/main_window.c
+++ src/windows/main_window.c
@@ -1,7 +1,5 @@
 #include "main_window.h"
 
-#define DEG_TO_TRIGANGLE(angle) ((angle) * (TRIG_MAX_ANGLE / 360))
-
 static Window *s_window;
 static Layer *s_canvas;
 
@@ -17,14 +15,21 @@ static void layer_update_proc(Layer *layer, GContext *ctx) {
   // Minutes are expanding circle arc
   int radius = MINUTES_RADIUS;
   int minute_angle = (int)(((float)s_minutes / 60.0F) * 360.0F);
+  GRect frame = GRect((bounds.size.w / 2) - radius, (bounds.size.h / 2) - radius, 
+                      2 * radius, 2 * radius);
   graphics_context_set_fill_color(ctx, MINUTES_COLOR);
-  graphics_fill_radial(ctx, center, radius - (6 * HOURS_RADIUS), radius, 0, DEG_TO_TRIGANGLE(minute_angle));
+  graphics_fill_radial(ctx, frame, GOvalScaleModeFillCircle, 20, 0, DEG_TO_TRIGANGLE(minute_angle));
+
+  // Adjust geometry variables for inner ring
+  frame.origin.x += 2 * HOURS_RADIUS;
+  frame.origin.y += 3 * HOURS_RADIUS;
+  frame.size.w -= 6 * HOURS_RADIUS;
+  frame.size.h -= 6 * HOURS_RADIUS;
 
   // Hours are dots
-  radius -= 3 * HOURS_RADIUS;
   for(int i = 0; i < 12; i++) {
     int hour_angle = (int)(((float)i / 12.0F) * 360.0F);
-    GPoint pos = gpoint_from_polar(&center, radius, DEG_TO_TRIGANGLE(hour_angle));
+    GPoint pos = gpoint_from_polar(frame, GOvalScaleModeFillCircle, DEG_TO_TRIGANGLE(hour_angle));
 
     graphics_context_set_fill_color(ctx, i <= s_hours ? HOURS_COLOR : HOURS_COLOR_INACTIVE);
     graphics_fill_circle(ctx, pos, HOURS_RADIUS);
